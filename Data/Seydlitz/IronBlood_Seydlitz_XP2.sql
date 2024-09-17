53926dbb83c695d6af138d6201030b3d2e4543da
HSbF5HSO3F <jinghaoyu1030@qq.com>
2024-09-17 22:12:11 +0800

    塞德利茨：添加xp2更新

---
+++ Data/Seydlitz/IronBlood_Seydlitz_XP2.sql
@@ -0,0 +1,18 @@
+-- IronBlood_Seydlitz_XP2
+-- Author: jjj
+-- DateCreated: 2024/9/17 21:59:42
+--------------------------------------------------------------
+--Units_XP2
+INSERT INTO Units_XP2
+		(UnitType,		        ResourceCost,	ResourceMaintenanceType,ResourceMaintenanceAmount)
+VALUES	('UNIT_SMS_SEYDLITZ',	1,				'RESOURCE_OIL',			1);
+
+--Battle Ship
+UPDATE Units
+SET PrereqTech = 'TECH_REFINING'
+WHERE UnitType = 'UNIT_SMS_SEYDLITZ';
+
+--Update
+UPDATE Traits
+SET Description = 'LOC_TRAIT_LEADER_UNSINKABLE_LEGEND_DESCRIPTION_XP2'
+WHERE TraitType = 'TRAIT_LEADER_UNSINKABLE_LEGEND';
